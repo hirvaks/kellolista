19e702fcb0a57f994e1b4e03d39effe4e609acaa
Akseli Hirvelä <akseli.hirvela@gmail.com>
2022-04-14 16:29:27 +0300

    disabling CORS, try3

--- src/main/java/com/example/watchList/web/WatchController.java
+++ src/main/java/com/example/watchList/web/WatchController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,7 @@ public class WatchController {
 	@Autowired
 	private WatchRepository wrepository;
 	
+	@CrossOrigin
 	@RequestMapping(value="/watches", method = RequestMethod.GET)
 	public @ResponseBody List<Watch> watchListRest() {
 		return (List<Watch>) wrepository.findAll();
